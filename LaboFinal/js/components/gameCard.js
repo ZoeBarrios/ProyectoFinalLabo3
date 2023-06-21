af81741466812df7e46f0757614ea8ab4c486444
ZoeBarrios <zbarrios@frsn.utn.edu.ar>
2023-06-21 20:28:40 -0300

    Arreglando detalles del hover de las cards

--- LaboFinal/js/components/gameCard.js
+++ LaboFinal/js/components/gameCard.js
@@ -1,28 +1,33 @@
 export function createGameCard(game) {
   const { ratings } = game;
+
   const contenedorLista = document.createElement("div");
   contenedorLista.classList.add("contenedorLista");
+  contenedorLista.style.display = "none";
+
   const ulEl = document.createElement("ul");
   ratings.forEach((rating) => {
-    ulEl.innerHTML += `<li>${rating.title} ${rating.percent}</li>`;
+    const liEl = document.createElement("li");
+    liEl.textContent = `${rating.title} ${rating.percent}`;
+    ulEl.appendChild(liEl);
   });
+
+  contenedorLista.appendChild(ulEl);
 
   const gameEl = document.createElement("div");
   gameEl.classList.add("game");
   gameEl.innerHTML = `
-  <div id="card">
+    <div id="card">
       <img src="${game.background_image}" alt="${game.name}" class="game-img" />
-      <div class="contenedorLista">
-      ${ulEl.outerHTML}
-      </div>
+      ${contenedorLista.outerHTML}
       <div class="game-info">
         <a href="../html/game.html?id=${game.id}">Ver más</a>
         <h3>${game.name}</h3>
         <span>${game.released}</span>
         <span>RATING ${game.rating}</span>
       </div>
-      </div>
-    `;
+    </div>
+  `;
 
   return gameEl;
 }
