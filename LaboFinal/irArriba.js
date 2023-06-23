25bb45c65dacbb3727f11100b63e5e943db5e8c1
ZoeBarrios <zbarrios@frsn.utn.edu.ar>
2023-06-23 12:16:45 -0300

    Cambiando de lugar el jquery en game.html

--- LaboFinal/irArriba.js
+++ LaboFinal/irArriba.js
@@ -1,18 +1,19 @@
 /* IR ARRIBA */
-$(document).ready(function(){
+$(document).ready(function () {
+  $(".ir-arriba").click(function () {
+    $("body, html").animate(
+      {
+        scrollTop: "0px",
+      },
+      300
+    );
+  });
 
-	$('.ir-arriba').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
-
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.ir-arriba').slideDown(300);
-		} else {
-			$('.ir-arriba').slideUp(300);
-		}
-	});
-
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".ir-arriba").slideDown(300);
+    } else {
+      $(".ir-arriba").slideUp(300);
+    }
+  });
 });
