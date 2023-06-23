e073a6a1571f44d697c8e72b5a62d409f8546299
ZoeBarrios <zbarrios@frsn.utn.edu.ar>
2023-06-23 13:43:07 -0300

    Separando los token en constantes

--- LaboFinal/js/vercelVKFuntions.js
+++ LaboFinal/js/vercelVKFuntions.js
@@ -1,13 +1,12 @@
+const URL_VK = import.meta.env.VITE_KV_REST_API_URL;
+const TOKEN_VK = import.meta.env.VITE_KV_REST_API_TOKEN;
 export async function getAll(key) {
   let all = [];
-  let elements = await fetch(
-    `${import.meta.env.VITE_KV_REST_API_URL}/get/${key}`,
-    {
-      headers: {
-        Authorization: `Bearer ${import.meta.env.VITE_KV_REST_API_TOKEN}`,
-      },
-    }
-  ).then((response) => response.json());
+  let elements = await fetch(`${URL_VK}/get/${key}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_KV_REST_API_TOKEN}`,
+    },
+  }).then((response) => response.json());
 
   if (elements.result) {
     all = JSON.parse(elements.result);
@@ -17,9 +16,9 @@ export async function getAll(key) {
 }
 
 export async function pushDB(key, data) {
-  await fetch(`${import.meta.env.VITE_KV_REST_API_URL}/set/${key}`, {
+  await fetch(`${URL_VK}/set/${key}`, {
     headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_KV_REST_API_TOKEN}`,
+      Authorization: `Bearer ${TOKEN_VK}`,
     },
     body: JSON.stringify(data),
     method: "POST",
